70a99163004c06448578483ca176fb8d89bc3c60
Johnatan Hallel <jhallel@gmail.com>
2020-06-27 19:18:37 +0300

    ui-add-navbar

--- src/app/logistics/logistics.module.ts
+++ src/app/logistics/logistics.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LogisticsRoutingModule } from './logistics-routing.module';
-import { LogisticsComponent } from './logistics.component';
+// import { LogisticsComponent } from './logistics.component';
 
 
 @NgModule({
-  declarations: [LogisticsComponent],
+  // declarations: [LogisticsComponent],
   imports: [
     CommonModule,
     LogisticsRoutingModule
