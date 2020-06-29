22bc2cb734865caca2c83d5ea06fe4924bb74c61
Itamar Priel <itamar.priel@gmail.com>
2020-06-29 18:24:36 +0300

    * Setup initial implementation of auth system * Setup Firebase auth * Implement Auth Guard that checks for a user role * Move 'log-in' to 'login'

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,14 +1,31 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LogInComponent } from './log-in/log-in.component';
+import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { RouterModule } from '@angular/router';
+import { environment } from '../../environments/environment';
+import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
-
-
 @NgModule({
-  declarations: [LogInComponent, RegisterComponent],
+  declarations: [LoginComponent, RegisterComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule,
+    MatCardModule,
+    MatCheckboxModule
   ]
 })
 export class AuthModule { }
---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,65 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
+import { AuthModule } from './auth.module';
+
+@Injectable({
+  providedIn: AuthModule
+})
+export class AuthService {
+
+  async getUserClaims() {
+    const res = await auth().currentUser.getIdTokenResult();
+    return res.claims;
+  }
+
+  doLocalLogin(email: string, password: string): Promise<auth.UserCredential> {
+    return new Promise<any>((resolve, reject) => {
+      auth().signInWithEmailAndPassword(email, password)
+        .then(res => {
+          resolve(res);
+        }, err => {
+          console.log(err);
+          reject(err);
+        });
+    });
+  }
+
+  doGoogleLogin(): Promise<auth.UserCredential> {
+    let provider = new auth.GoogleAuthProvider();
+    provider.addScope('profile');
+    provider.addScope('email');
+    return this.doSocialLogin(provider);
+  }
+
+  doFacebookLogin(): Promise<auth.UserCredential> {
+    const provider = new auth.FacebookAuthProvider();
+    return this.doSocialLogin(provider);
+  }
+
+  doTwitterLogin(): Promise<auth.UserCredential> {
+    const provider = new auth.TwitterAuthProvider();
+    return this.doSocialLogin(provider);
+  }
+
+  private doSocialLogin(provider: auth.AuthProvider): Promise<auth.UserCredential> {
+    return this.afAuth.signInWithPopup(provider)
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  logout() {
+    return new Promise<any>((resolve, reject) => {
+      auth().signOut()
+        .then(res => {
+          resolve(null);
+        }, err => {
+          console.log(err);
+          reject(err);
+        });
+    });
+  }
+
+  constructor(private afAuth: AngularFireAuth) { }
+}
