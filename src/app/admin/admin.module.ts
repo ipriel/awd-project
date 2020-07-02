69efd5ba3a7bbc45c6f88cf3e4c3da5e716a4174
ItayBeresi <50947234+ItayBeresi@users.noreply.github.com>
2020-07-02 15:40:09 +0300

    new dashboard component
    Created dashboard component from admin

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 // import { AdminComponent } from './admin.component';
 
 
@@ -10,6 +11,7 @@ import { AdminRoutingModule } from './admin-routing.module';
   imports: [
     CommonModule,
     AdminRoutingModule
-  ]
+  ],
+  declarations: [DashboardComponent]
 })
 export class AdminModule { }
