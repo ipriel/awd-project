efeca5f8131fea1883f45645d14cc1042618baee
Johnatan Hallel <jhallel@gmail.com>
2020-07-05 15:12:53 +0300

    add some things to checkout

--- src/app/store/product/shopping-cart/checkout/checkout.component.ts
+++ src/app/store/product/shopping-cart/checkout/checkout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
@@ -6,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout.component.css']
 })
 export class CheckoutComponent implements OnInit {
-
+  isLinear = true;
+  firstFormGroup:FormGroup;
   constructor() { }
 
   ngOnInit(): void {
