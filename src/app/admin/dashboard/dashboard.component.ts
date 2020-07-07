9ec45bd58b0b3b36a51ad4ddad57880dbebe470a
ItayBeresi <50947234+ItayBeresi@users.noreply.github.com>
2020-07-07 14:38:13 +0300

    Update dashboard.component.ts
    using admin service to get values for cards in ngOnInit.

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  todaysIncome;
+  productsSum;
+  registeredUsers;
+  onlinePeople;
+
+  constructor(private adminService: AdminService) { }
 
   ngOnInit(): void {
+    // get today's income
+    this.adminService.getTodaysIncome()
+      .subscribe( value => { this.todaysIncome = value; });
+
+    // get products sum
+    this.adminService.getProductsSum()
+      .subscribe( value => { this.productsSum = value; });
+
+    // get registered users
+    this.adminService.getRegisteredUsers()
+      .subscribe( value => { this.registeredUsers = value; });
+
+    // get online people
+    this.adminService.getOnlinePeople()
+      .subscribe( value => { this.onlinePeople = value; });
   }
+
+
 
 }
