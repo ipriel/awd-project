bd061efecf759325d21c9106124c39bf7dceb43f
ItayBeresi <50947234+ItayBeresi@users.noreply.github.com>
2020-07-15 12:00:34 +0300

    Updated Admin Product page
    * Installed sweetalert2
    
    * made basic pages for meta product and shop product
    
    * got some issues with displaying specs, currently at work

--- src/app/admin/products-editor/products-editor.component.ts
+++ src/app/admin/products-editor/products-editor.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from "@angular/common";
 import { Observable } from 'rxjs';
 import { stringify } from 'querystring';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-products-editor',
@@ -12,11 +13,26 @@ export class ProductsEditorComponent implements OnInit {
 
   private btnDisable: boolean = true;
   private productSelected;
-  selectedType: number = 0;
+  selectedType: number = 0; // either Shop or Meta
+  selectedProduct; // product ID
+  currentProduct;
   showProduct: boolean = false;
   showMeta: boolean = false;
-  updateProduct: boolean = false;
-  currentProduct;
+
+  // shop variables
+  updatingProduct: boolean = false;
+
+  // meta variables
+  creatingProduct: boolean = false;
+  oldName: string;
+  oldType: string;
+  oldCompany: string;
+  oldPrice: number;
+  oldSpecs: Array<any> = [];
+  newDescription: string = "";
+  newDiscount: number = 0;
+  newQuantity: number = 0;
+  newShowInStore: boolean = false;
 
   options : Array<any> = [];
 
@@ -29,7 +45,7 @@ export class ProductsEditorComponent implements OnInit {
     this.options = [];
     this.showMeta = false;
     this.showProduct = false;
-    this.updateProduct = false;
+    this.updatingProduct = false;
 
     if (this.selectedType == 1) // Search shop products
     {
@@ -47,30 +63,126 @@ export class ProductsEditorComponent implements OnInit {
 
   findProduct(productId): void {
     // search product
-    this.currentProduct = {
-      name: "Samsung Galaxy",
-      type: 'Phone',
-      company: "Samsung",
-      price: "400",
-      description: "this is a phone",
-      discount: 0,
-      quantity: 5,
-      specs: [
-        { title: "x", value: 5 },
-        { title: "y", value: 6 }],
-      showInStore: true
-    };
 
     // if product found
     if (this.selectedType == 1)    
+    {
+      this.currentProduct = {
+        name: "Samsung Galaxy",
+        type: 'Phone',
+        company: "Samsung",
+        price: "400",
+        description: "this is a phone",
+        discount: 0,
+        quantity: 5,
+        specs: [
+          { title: "x", value: 5 },
+          { title: "y", value: 6 }],
+        showInStore: true
+      };
+
       this.showProduct = true;
+    }
     else if (this.selectedType == 2)
+    {
+      this.currentProduct = {
+        name: "Samsung Galaxy",
+        type: "Phone",
+        company: "Samsung",
+        price: 350,
+        specs: [
+          { title: "x", value: 5 },
+          { title: "y", value: 6 }]
+      }
+
+      this.oldName = this.currentProduct.name;
+      this.oldType = this.currentProduct.type;
+      this.oldCompany = this.currentProduct.company;
+      this.oldPrice = this.currentProduct.price;
+      this.oldSpecs = this.currentProduct.specs;
+
       this.showMeta = true;
+    }
 
     // else
   }
 
+  delete()
+  {
+    swal.fire({
+      title: 'Are you sure?',
+      text: 'You will not be able to recover once you delete it.',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes',
+      cancelButtonText: 'No!'
+    }).then((result) => {
+      if (result.value) {
+        swal.fire(
+          'Deleted!',
+          'The product was deleted from the shop database',
+          'success'
+        )
+      }
+    });
+  }
 
+  updateProduct()
+  {
+    this.updatingProduct = true;
+  }
+
+  cancelUpdate()
+  {
+    this.updatingProduct = false;
+  }
+
+  saveUpdate() 
+  {
+    this.updatingProduct = false;
+  }
+
+  createNewProduct()
+  {
+    this.creatingProduct = true;
+  }
+
+  saveCreate()
+  {
+    swal.fire({
+      title: 'Are you sure?',
+      text: 'Your new product will be saved in the shop database.',
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonText: 'Yes',
+      cancelButtonText: 'No'
+    }).then((result) => {
+      if (result.value) {
+        this.creatingProduct = false;
+
+        swal.fire(
+          'Saved!',
+          'The product was in the shop database',
+          'success'
+        )
+      }
+    });
+  }
+
+  cancelCreate()
+  {
+    this.creatingProduct = false;
+
+    this.currentProduct.name = this.oldName;
+    this.currentProduct.type = this.oldType;
+    this.currentProduct.company = this.oldCompany;
+    this.currentProduct.price = this.oldPrice;
+    this.currentProduct.specs = this.oldSpecs;
+    this.newDiscount = 0;
+    this.newDescription = "";
+    this.newQuantity = 0;
+    this.newShowInStore = false;
+  }
 
   isOptionsEmpty() : boolean {
     if (this.options.length > 0) return false;
