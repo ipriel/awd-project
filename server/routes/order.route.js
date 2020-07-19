2244d27b3a2cd4fc8e22bc2ca53e389304300d76
Itamar Priel <itamar.priel@gmail.com>
2020-07-19 13:23:36 +0300

    Fix errors in aggregate function

--- server/routes/order.route.js
+++ server/routes/order.route.js
@@ -14,17 +14,6 @@ router.post('/', verifyToken, (req, res) => {
     });
 });
 
-router.get('/analytics/income/daily', verifyToken, hasRole('admin'), (req, res) => {
-    Order.aggregate([
-        { $group: { name: { $dateToString: { format: "%d/%m", date: "$date" } }, value: { $sum: "$totalPrice" } } }
-    ]).exec((err, docs) => {
-        if (err) {
-            return res.status(400).send(err);
-        }
-        res.send({data: docs});
-    });
-});
-
 // Update
 router.put('/:id', verifyToken, hasRole('logistics'), (req, res) => {
     Order.findByIdAndUpdate(req.params.id, req.body, (err, doc) => {
@@ -46,10 +35,47 @@ router.delete('/:id', verifyToken, isUser, (req, res) => {
 });
 
 // Read
-router.get('/analytics/income/today', verifyToken, hasRole('admin'), (req, res) => {
+router.get('/analytics/income/daily', /*verifyToken, hasRole('admin'),*/ (req, res) => {
     Order.aggregate([
-        { $match: { date: {$eq: new Date(),} } },
-        { $group: { income: { $sum: "$totalPrice" } } }
+        { 
+            $group: {
+                _id: { $dateToString: { format: '%d-%m', date: '$date' } },
+                value: { $sum: '$totalPrice' }
+            }
+        },
+        {
+            $project: {
+                name: '_id',
+                value: 1,
+                _id: 0
+            }
+        }
+    ]).exec((err, docs) => {
+        if (err) {
+            return res.status(400).send(err);
+        }
+
+        if(docs.length == 0) {
+            const date = new Date();
+            docs.push({name: date.getDate()+'-'+(date.getMonth()+1), value: 0});
+        }
+        res.send(docs);
+    });
+});
+
+router.get('/analytics/income/today', /* verifyToken, hasRole('admin'), */ (req, res) => {
+    Order.aggregate([
+        {
+            $match: {
+                date: { $eq: new Date() }
+            }
+        },
+        {
+            $group: {
+                _id: '$date',
+                income: { $sum: "$totalPrice" }
+            }
+        }
     ]).exec((err, doc) => {
         if (err) {
             return res.status(400).send(err);
